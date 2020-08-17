1cc6c9f4e450d2dc1688a4c48fe23e3337cf8b92
Alexandre GILBERT <alexandre.gilbert66@gmail.com>
2020-08-17 15:41:35 -0400

    Initial Commit

---
+++ Clean CityTests/Clean_CityTests.swift
@@ -0,0 +1,34 @@
+//
+//  Clean_CityTests.swift
+//  Clean CityTests
+//
+//  Created by Alexandre GILBERT on 17/08/2020.
+//  Copyright © 2020 Alexandre GILBERT. All rights reserved.
+//
+
+import XCTest
+@testable import Clean_City
+
+class Clean_CityTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
